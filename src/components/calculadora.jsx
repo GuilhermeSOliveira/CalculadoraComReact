7b4e9e08056b0d5b91fce3068a07615b926beba7
Guilherme <guih_so@hotmail.com>
2022-04-20 20:18:31 -0300

    Finalizando design

--- src/components/calculadora.jsx
+++ src/components/calculadora.jsx
@@ -5,23 +5,27 @@ export default function Calculator() {
     return (
         <div className="container">
             <div className="background">
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>+</button>
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                <button>-</button>
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
-                <button>*</button>
-                <button>del</button>
-                <button>0</button>
-                <button>.</button>
-                <button>/</button>
-                <button>reset</button>
+                <div className="box">
+                    <h1 className="resultado">01</h1>
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>+</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>-</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button>*</button>
+                    <button>del</button>
+                    <button>0</button>
+                    <button>.</button>
+                    <button>/</button>
+                    <button>reset</button>
+                    <button>=</button>
+                </div>
             </div>
         </div>
     )
