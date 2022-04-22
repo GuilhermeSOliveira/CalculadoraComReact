419ad9ccfd880bd9bc2b0b1cf7de8043019c3f9c
Guilherme <guih_so@hotmail.com>
2022-04-22 20:49:52 -0300

    Deixando a calculadora funcional

--- src/components/calculadora.jsx
+++ src/components/calculadora.jsx
@@ -3,6 +3,7 @@ import './calculator.css'
 
 export default function Calculator() {
     const [num, setNum] = useState('')
+    const tela = document.querySelector('.resultado')
 
     function inputNum (e) {
         var input = e.target.value
@@ -12,6 +13,17 @@ export default function Calculator() {
     function clear () {
         setNum('')
     }
+
+    function back () {
+        setNum(num.substring(0, num.length -1))
+    }
+
+    function calcular () {
+        let resultado = tela.innerHTML;
+        if(resultado){
+            tela.innerHTML = eval(resultado);
+        }
+    }
     return (
         <div className="container">
             <div className="background">
@@ -20,21 +32,21 @@ export default function Calculator() {
                     <button onClick={inputNum} value={1}>1</button>
                     <button onClick={inputNum} value={2}>2</button>
                     <button onClick={inputNum} value={3}>3</button>
-                    <button>+</button>
+                    <button onClick={inputNum} value={"+"}>+</button>
                     <button onClick={inputNum} value={4}>4</button>
                     <button onClick={inputNum} value={5}>5</button>
                     <button onClick={inputNum} value={6}>6</button>
-                    <button>-</button>
+                    <button onClick={inputNum} value={"-"}>-</button>
                     <button onClick={inputNum} value={7}>7</button>
                     <button onClick={inputNum} value={8}>8</button>
                     <button onClick={inputNum} value={9}>9</button>
-                    <button>*</button>
-                    <button>del</button>
+                    <button onClick={inputNum} value={"*"}>*</button>
+                    <button onClick={back}>C</button>
                     <button onClick={inputNum} value={0}>0</button>
-                    <button>.</button>
-                    <button>/</button>
+                    <button onClick={inputNum} value={"."}>.</button>
+                    <button onClick={inputNum} value={"/"}>/</button>
                     <button onClick={clear}>reset</button>
-                    <button>=</button>
+                    <button onClick={calcular}>=</button>
                 </div>
             </div>
         </div>
