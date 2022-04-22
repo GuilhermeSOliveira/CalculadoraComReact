2a3b780b4df247f7851fea2c56fb199cdb52eec3
Guilherme <guih_so@hotmail.com>
2022-04-21 21:39:34 -0300

    Adicionando funcao para inserir os numeros e resetar

--- src/components/calculadora.jsx
+++ src/components/calculadora.jsx
@@ -1,29 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import './calculator.css'
 
 export default function Calculator() {
+    const [num, setNum] = useState('')
+
+    function inputNum (e) {
+        var input = e.target.value
+        setNum(num + input)
+    }
+
+    function clear () {
+        setNum('')
+    }
     return (
         <div className="container">
             <div className="background">
                 <div className="box">
-                    <h1 className="resultado">01</h1>
-                    <button>1</button>
-                    <button>2</button>
-                    <button>3</button>
+                    <h1 className="resultado">{num}</h1>
+                    <button onClick={inputNum} value={1}>1</button>
+                    <button onClick={inputNum} value={2}>2</button>
+                    <button onClick={inputNum} value={3}>3</button>
                     <button>+</button>
-                    <button>4</button>
-                    <button>5</button>
-                    <button>6</button>
+                    <button onClick={inputNum} value={4}>4</button>
+                    <button onClick={inputNum} value={5}>5</button>
+                    <button onClick={inputNum} value={6}>6</button>
                     <button>-</button>
-                    <button>7</button>
-                    <button>8</button>
-                    <button>9</button>
+                    <button onClick={inputNum} value={7}>7</button>
+                    <button onClick={inputNum} value={8}>8</button>
+                    <button onClick={inputNum} value={9}>9</button>
                     <button>*</button>
                     <button>del</button>
-                    <button>0</button>
+                    <button onClick={inputNum} value={0}>0</button>
                     <button>.</button>
                     <button>/</button>
-                    <button>reset</button>
+                    <button onClick={clear}>reset</button>
                     <button>=</button>
                 </div>
             </div>
